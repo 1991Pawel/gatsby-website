cb1ce798f4f4df8d3c0c87c1bc7e70cc4dc33d9e
Pawel L <virtuozoo@o2.pl>
2020-06-06 20:36:46 +0200

    add gsap animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useRef, useEffect } from "react"
+import gsap from "gsap"
 import styled from "styled-components"
 import photo1 from "../assets/image/photo1.jpg"
 import photo2 from "../assets/image/photo2.jpg"
@@ -11,7 +12,6 @@ import photo7 from "../assets/image/photo7.jpg"
 const PageWrapper = styled.div`
   display: flex;
   flex-direction: column;
-
   @media (min-width: 576px) {
     flex-direction: row;
   }
@@ -24,9 +24,13 @@ const ContentWrapper = styled.div`
   justify-content: flex-start;
   align-items: center;
 
-  @media (min-width: 576px) {
-    justify-self: flex-end;
-    width: 70%;
+  h1 {
+    @media (min-width: 576px) {
+      font-size: 3rem;
+    }
+    @media (min-width: 860px) {
+      font-size: 4rem;
+    }
   }
 `
 
@@ -43,9 +47,14 @@ const ImageWrapper = styled.div`
   }
 
   @media (min-width: 860px) {
-    grid-gap: 10px;
+    grid-gap: 8px;
     grid-template-columns: 130px 130px 60px 60px 60px 130px;
     grid-template-rows: 40px 60px 50px 50px 100px 180px;
+  }
+  @media (min-width: 1200px) {
+    grid-gap: 10px;
+    grid-template-columns: 150px 150px 80px 80px 80px 150px;
+    grid-template-rows: 60px 80px 70px 60px 120px 200px;
   }
 
   img {
@@ -60,20 +69,18 @@ const ImageWrapper = styled.div`
   img:nth-of-type(1) {
     grid-column: 4 / span 1;
     grid-row: 1 / span 1;
-
+    transform: translateY(15%);
     @media (min-width: 576px) {
       grid-row: 1 / span 2;
       grid-column: 6 / span 1;
+      transform: translateY(0%);
     }
   }
   img:nth-of-type(2) {
     grid-column: 3 / span 2;
     grid-row: 2 / span 1;
-    transform: translateY(-20px);
     z-index: -2;
-
     @media (min-width: 576px) {
-      transform: translateY(0px);
       grid-column: 6 / span 1;
       grid-row: 3 / span 3;
       border-radius: 0;
@@ -83,11 +90,11 @@ const ImageWrapper = styled.div`
     grid-column: 2 / span 3;
     grid-row: 3 / span 1;
     z-index: -3;
-    transform: translateY(-40px);
+    transform: translateY(-15%);
     @media (min-width: 576px) {
-      transform: translateY(0px);
       grid-row: 4 / span 2;
       grid-column: 3 / span 3;
+      transform: translateY(0%);
     }
   }
 
@@ -95,9 +102,7 @@ const ImageWrapper = styled.div`
     display: none;
     @media (min-width: 576px) {
       display: block;
-
       grid-row: 2 / span 2;
-
       grid-column: 4 / span 2;
     }
   }
@@ -129,24 +134,60 @@ const ImageWrapper = styled.div`
   }
 `
 
-const IndexPage = () => (
-  <PageWrapper>
-    <ContentWrapper>
-      <div>
-        <h1>John Doe</h1>
-        <p>Welcome on my photography portfolio.</p>
-      </div>
-    </ContentWrapper>
-    <ImageWrapper>
-      <img src={photo1} alt="photography" />
-      <img src={photo2} alt="photography" />
-      <img src={photo7} alt="photography" />
-      <img src={photo6} alt="photography" />
-      <img src={photo5} alt="photography" />
-      <img src={photo3} alt="photography" />
-      <img src={photo4} alt="photography" />
-    </ImageWrapper>
-  </PageWrapper>
-)
+const IndexPage = () => {
+  const image1 = useRef(null)
+  const image2 = useRef(null)
+  const image3 = useRef(null)
+  const image4 = useRef(null)
+  const image5 = useRef(null)
+  const image6 = useRef(null)
+  const image7 = useRef(null)
+
+  useEffect(() => {
+    gsap.fromTo([image1.current], { opacity: 0 }, { opacity: 1, duration: 0.8 })
+    gsap.fromTo([image2.current], { opacity: 0 }, { delay: 0.3, opacity: 1 })
+    gsap.fromTo([image3.current], { opacity: 0 }, { delay: 0.7, opacity: 1 })
+    gsap.fromTo(
+      [image4.current],
+      { opacity: 0, x: -50 },
+      { delay: 0.8, opacity: 1, duration: 1, x: 0 }
+    )
+    gsap.fromTo(
+      [image5.current],
+      { opacity: 0, x: 50 },
+      { opacity: 1, duration: 1, x: 0 }
+    )
+    gsap.fromTo(
+      [image6.current],
+      { opacity: 0, x: 50 },
+      { delay: 1, opacity: 1, duration: 1, x: 0 }
+    )
+    gsap.fromTo(
+      [image7.current],
+      { opacity: 0, x: -50 },
+      { delay: 0.5, opacity: 1, duration: 1, x: 0 }
+    )
+  }, [])
+
+  return (
+    <PageWrapper>
+      <ContentWrapper>
+        <div>
+          <h1>John Doe</h1>
+          <p>Welcome on my photography portfolio.</p>
+        </div>
+      </ContentWrapper>
+      <ImageWrapper>
+        <img ref={image1} src={photo1} alt="photography" />
+        <img ref={image2} src={photo2} alt="photography" />
+        <img ref={image3} src={photo7} alt="photography" />
+        <img ref={image4} src={photo6} alt="photography" />
+        <img ref={image5} src={photo5} alt="photography" />
+        <img ref={image6} src={photo3} alt="photography" />
+        <img ref={image7} src={photo4} alt="photography" />
+      </ImageWrapper>
+    </PageWrapper>
+  )
+}
 
 export default IndexPage
